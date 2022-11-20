b4ec67cedc5510ed43682108fe8804218b45f9c3
Levi <dragonball14499@gmail.com>
2022-11-20 22:19:22 +0200

    Set X and Y position in the style of button depending on the result

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ btnEl.addEventListener("mouseover", (event) => {
     const x = event.pageX - btnEl.offsetLeft;
     const y = event.pageY - btnEl.offsetTop;
 
+    btnEl.style.setProperty("--xPos", x + "px");
+    btnEl.style.setProperty("--yPos", y + "px");
 });
